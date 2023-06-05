e101d08a46479a87e7f7b9578068efbc6cffd5c3
jayzadafiya <zadafiyajay2@gmail.com>
2023-06-05 15:30:45 +0530

    create view for homeand user

--- controllers/homne_controller.js
+++ controllers/homne_controller.js
@@ -1,6 +1,9 @@
 // in large scale project controller is set of action 
 
 
-module.exports.home = function(req,res){
- return res.end("<h1>Epress is up for Codeial</h1>")   
+module.exports.home = function (req, res) {
+    //  return res.end("<h1>Epress is up for Codeial</h1>")   
+    return res.render('home',{
+        title: "Home",
+    })
 }
