0333e864e89e6c5fdcff4170b70353e3115f0fb6
jayzadafiya <zadafiyajay2@gmail.com>
2023-06-14 01:00:28 +0530

    Display posts and user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,20 @@
 // in large scale project controller is set of action 
+const Post = require("../models/post");
 
-
-module.exports.home = function (req, res) {
-    //  return res.end("<h1>Epress is up for Codeial</h1>")   
-    return res.render('home',{
-        title: "Home",
-    })
+module.exports.home = async function (req, res) {
+    // //  return res.end("<h1>Epress is up for Codeial</h1>")   
+    // return res.render('home',{
+    //     title: "Home",
+    // })
+    try {
+        //   const posts =   await Post.find({});
+        //populater user for each
+        const posts = await Post.find({}).populate('user').exec();
+        return res.render('home', {
+            title: 'Codeial | Home',
+            posts: posts
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
