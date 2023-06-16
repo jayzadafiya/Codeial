19fe2d41c266584797fb0925f571c8f21a010acb
jayzadafiya <zadafiyajay2@gmail.com>
2023-06-16 14:51:31 +0530

    check authentication and display form

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const passport   = require("passport");
 
 const postsController = require("../controllers/posts_controller")
-router.post("/create", postsController.create);
+// router.post("/create", postsController.create);
+
+// stop user for make change in code with inspect mode 
+router.post("/create",passport.checkAuthentication, postsController.create);
 
 module.exports = router;
