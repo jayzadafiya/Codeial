1425b19f6f13ef0092ba309a2fc28c7f78b413b5
jayzadafiya <zadafiyajay2@gmail.com>
2023-07-21 02:58:43 +0530

    send and receive message

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -16,6 +16,23 @@ module.exports.chatSockets=function(socketServer){
         socket.on("disconnect",function(){
             console.log("socket disconnect");
         })
+
+        //on method call when join_room is emit from chat engine(client)
+        socket.on("join_room",function(data){
+            console.log('joining request rec.',data);
+
+            socket.join(data.chatroom);//if room exist then user connet toit else room is create and enter user to it
+
+            // for emit in chat room we emit reques by io
+            io.in(data.chatroom).emit('user_joined',data);
+            //send notification of client to all other usersthet new user is enter in room
+        });
+
+        //detect send_message and broadcast to everyone in room
+        socket.on('send_message',function(data){
+            //within same chat room emit a event
+            io.in(data.chatroom).emit('receive_message',data); 
+        })  
     });
-    
+
 }
