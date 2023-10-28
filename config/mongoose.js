32d920fa704b82c1ccd59f3bd12c280ddb85936a
Jay Zadafiya <82803534+jayzadafiya@users.noreply.github.com>
2023-10-28 21:50:19 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://zadafiyajay2:HYXk63r0ukDejcSrcluster0.qvyojwi.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://zadafiyajay2:HYXk63r0ukDejcSrcluster0.qvyojwi.mongodb.net/?retryWrites=true&w=majority",, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
